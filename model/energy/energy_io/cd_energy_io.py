0a5226b8380fdecd2bd77475f118bb815375710b
Yuta A. Takagi <yutaatakagi@gmail.com>
2019-06-21 11:49:52 -0800

    Update cd_energy_io.py

--- model/energy/energy_io/cd_energy_io.py
+++ model/energy/energy_io/cd_energy_io.py
@@ -1,8 +1,17 @@
-# cd_energy_io.py
-from ... import global_variables
+#!/usr/bin/env python
+
+"""
+This file is part of Simmerpop which is released under the GNU General Purpose License version 3. 
+See COPYING or go to <https://www.gnu.org/licenses/> for full license details.
+"""
+
 import itertools
 
-__author__ = 'Yuta A. Takagi'
+from ... import global_variables
+
+__authors__ = ['Yuta A. Takagi', 'Diep H. Nguyen']
+__copyright__ = 'Copyright 2019, Goldman Lab'
+__license__ = 'GPLv3'
 
 
 # holds the class definition for the 'EnergyIO' parent class. see the 'custom_energy_io_template' module for details.

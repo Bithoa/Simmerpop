f9a2fd9ddc03eb87f26ed478394acc30bdb565c1
Yuta A. Takagi <yutaatakagi@gmail.com>
2020-03-30 12:50:08 -0800

    Update cellulator_food_io_with_cellularity_based_food.py

--- model/food/food_io/cellulator_food_io_with_cellularity_based_food.py
+++ model/food/food_io/cellulator_food_io_with_cellularity_based_food.py
@@ -9,6 +9,7 @@ import random
 
 from . import cd_food_io
 from ..food_types import food_type_not
+from ... import global_variables
 
 __authors__ = ['Yuta A. Takagi', 'Diep H. Nguyen']
 __copyright__ = 'Copyright 2019, Goldman Lab'
@@ -21,23 +22,63 @@ __license__ = 'GPLv3'
 # it provides a 'FoodNot' food every time an organism requests a food
 
 
+# the number of food in a newly added food parcel
+FOOD_CAP_PROPORTION = 1.5
+LOOSE_FOOD_POOL_CAP = 10000
+NEW_FOOD_PARCEL_SIZE = 5
+
+
 # initialize the food_type_not script
 def init_script():
 	food_type_not.init_script()
 
 
 class FoodIO(cd_food_io.FoodIO):  # this class name cannot be altered. Leave as 'FoodIO'  
+	def __init__(self):
+		global FOOD_CAP_PROPORTION
+		global LOOSE_FOOD_POOL_CAP
+		global NEW_FOOD_PARCEL_SIZE
+		self.loose_food_parcels = []
+
+		if 'FOOD_CAP_PROPORTION' in global_variables.parameters.keys():
+			FOOD_CAP_PROPORTION = float(global_variables.parameters.get('FOOD_CAP_PROPORTION'))
+		LOOSE_FOOD_POOL_CAP = int(FOOD_CAP_PROPORTION*global_variables.POPULATION_CAP)
+		if 'NEW_ENERGY_PARCEL_SIZE' in global_variables.parameters.keys():
+			NEW_FOOD_PARCEL_SIZE = int(global_variables.parameters.get('NEW_FOOD_PARCEL_SIZE'))
+	
 	def next_step(self):
 		food_type_not.calc_food_payoff_reduction_factor()
+		if global_variables.step_num % global_variables.MUTATION_INTERVAL == 0:
+			self.add_food()
+			self.cull_food()
+		
+	def add_food(self):
+		global NEW_FOOD_PARCEL_SIZE
+		global LOOSE_FOOD_POOL_CAP
+		while len(self.loose_food_parcels) <= LOOSE_FOOD_POOL_CAP:
+			temp = []
+			for i in range(NEW_FOOD_PARCEL_SIZE):
+				temp.append(food_type_not.FoodNot())
+			self.loose_food_parcels.append(temp)
+
+	def cull_food_pool(self):
+		global LOOSE_FOOD_POOL_CAP:
+		while len(self.loose_food_parcels) > LOOSE_FOOD_POOL_CAP:
+			remove_me = random.randint(0, len(self.loose_food_parcels) - 1)
+			self.loose_food_parcels.pop(remove_me)
 
 	def get_food(self):
-		return food_type_not.FoodNot()
+		temp = len(self.loose_food_parcels)
+		if temp > 0:
+			return self.loose_food_parcels.pop(random.randint(0, temp - 1))
+		else:
+			return []
 
 	def get_test_food(self):
 		return food_type_not.FoodNot()
 
 	def discard_food(self, food_to_discard):
-		pass
+		self.loose_food_parcels.append(food_to_discard)
 
 	def init_food_from_string_repr(self, string_repr):
 		repr_params = string_repr.split('_')
